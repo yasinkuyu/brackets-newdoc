3f2eca44bbfc9ef51d631aff1cc244c4b0173e9c
yasinkuyu <yasinkuyu@gmail.com>
2014-06-21 02:27:49 +0300

    init commit

---
+++ main.js
@@ -0,0 +1,98 @@
+/*
+ * Copyright (c) 2012 Adobe Systems Incorporated. All rights reserved.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a
+ * copy of this software and associated documentation files (the "Software"),
+ * to deal in the Software without restriction, including without limitation
+ * the rights to use, copy, modify, merge, publish, distribute, sublicense,
+ * and/or sell copies of the Software, and to permit persons to whom the
+ * Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+ * DEALINGS IN THE SOFTWARE.
+ *
+ * Yasin Kuyu - twitter.com/yasinkuyu
+ * Insya Interaktif
+ * 21/06/2014
+ *
+ */
+
+/*jslint vars: true, plusplus: true, devel: true, nomen: true, regexp: true, indent: 4, maxerr: 50 */
+/*global define, brackets, window, $, Mustache, navigator */
+
+define(function (require, exports, module) {
+    "use strict";
+
+    var docIndex         = 1;
+    var commandId        = "insya.newdoc.cmd";
+
+    // Menu
+    var html5File        = "insya.newhtml5";
+    var html5FileLabel   = "New Html5...";
+
+    // Brackets modules
+    var DocumentManager             = brackets.getModule("document/DocumentManager"),
+        Commands                    = brackets.getModule("command/Commands"),
+        CommandManager              = brackets.getModule("command/CommandManager"),
+        KeyBindingManager           = brackets.getModule("command/KeyBindingManager"),
+        EditorManager               = brackets.getModule("editor/EditorManager"),
+        Menus                       = brackets.getModule("command/Menus");
+
+    // Template file
+    var Html5Template               = require("text!htmlContent/template.html");
+
+    // Brackets elements
+    var sidebar = $("#sidebar"),
+        toolbar = $("#main-toolbar");
+
+ 	// Create Untitled Document function
+    function handleFileNew()
+	{
+        var doc = DocumentManager.createUntitledDocument(docIndex++, ".html");
+        DocumentManager.setCurrentDocument(doc);
+        EditorManager.focusEditor();
+        doc.setText(Html5Template);
+
+    }
+
+    // Sidebar double-click event
+    sidebar.on('dblclick', 'div', function(e) {
+
+        // if not children element
+        if (e.target === this) {
+            handleFileNew();
+        }
+
+    });
+
+    // Toolbar double-click event
+    toolbar.on('dblclick', function(e) {
+
+        // if not children element
+        if (e.target === this) {
+            handleFileNew();
+        }
+
+    });
+
+    // Register command
+    CommandManager.register(html5FileLabel, html5File, handleFileNew);
+
+    // Set menu
+    var menu = Menus.getMenu(Menus.AppMenuBar.FILE_MENU);
+    menu.addMenuItem(html5File, undefined, Menus.AFTER, Commands.FILE_NEW_UNTITLED);
+
+    // Shortcut add binding
+    KeyBindingManager.addBinding(commandId, "Ctrl-Alt-N", "mac");
+    KeyBindingManager.addBinding(commandId, "Ctrl-Alt-N", "win");
+
+
+});
